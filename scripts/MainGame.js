61089e39e4186a9a9d05e3eb5290644679a51380
tward1991 <w1616543@student.southstaffs.ac.uk>
2017-05-18 11:37:05 +0100

    changed spelling mistake

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -12,7 +12,7 @@ BasicGame.Game.prototype = {
 			//Add the ship onto the screen, set physics and the boundaries
 			ship = this.add.sprite((this.world.width / 2), this.world.height - 50, 'ship');
 			ship.anchor.setTo(0.5,0);
-			this.physics.enable(ship,Phaster.Physics.ARCADE)
+			this.physics.enable(ship,Phaser.Physics.ARCADE)
 			ship.body.collideWorldBounds= true;
 	},
 
