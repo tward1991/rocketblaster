2c4ac40c466d16ea858e331b44a4c7ffae198a8d
tward1991 <w1616543@student.southstaffs.ac.uk>
2017-05-18 11:14:29 +0100

    Javascript updated
    MainGame
    MainMenu
    Preloader

--- scripts/Preloader.js
+++ scripts/Preloader.js
@@ -7,15 +7,27 @@ BasicGame.Preloader = function (game) {
 BasicGame.Preloader.prototype = {
 
 	preload: function () {
-        
-	},
+    //displays a loading screen message while the assets are loaded into memory
+		this.preloaderText = this.add.text(this.world.centerX,this.world.centerY, 'Loading...',
+	{
+		fontSize: '96px'
+	  fill: '#fff',
+		align: 'center'
+	});
+	this.preloaderText.anchor.setTo(0.5,0.5);
 
+	//preload the images, sprites and audio assets into memory
+this.load.image('logo','assets/PhaserLogo.png');
+this.load.image('logo','assets/starfield.png');
+this.load.image('logo','assets/StartButton.png');
+this.load.image('logo','assets/ship.png');
+},
 	create: function () {
-	
+
 	},
 
 	update: function () {
-
+this.game.state.start('MainMenu');
 	}
 
 };
